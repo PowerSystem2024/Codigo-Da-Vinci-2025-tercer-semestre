db74346d70e7da10adbb8a09bcdfc00583d19dfb
mirenecarmen <carmenirenemachuca@gmail.com>
2025-05-04 18:16:34 -0300

    Agrego clase4 de JavaScript desde rama_irene

---
+++ primer cuatrimestre/javascript/clase4/manejoDeErrores/tryCatch.js
@@ -0,0 +1,14 @@
+'use strict';
+// usamos el bloque try-catch para evitar errores
+try{
+   let x= 10;  // lo traemos con alt + flecha hacia arriba o abajo
+    mifuncion();
+}
+catch(error){ // catchamos el error
+    console.log(error);
+}
+finally{
+    console.log('termina la revision de errores');
+}
+//la ejecucion ahora continua
+
